56d98adfe4c8759e9aa80ebab57758d92d312b97
chriseth <chris@ethereum.org>
2019-09-11 16:30:47 +0200

    Use builtin iszero for for loop condition rewriting.

--- libyul/optimiser/Suite.cpp
+++ libyul/optimiser/Suite.cpp
@@ -33,6 +33,7 @@
 #include <libyul/optimiser/ExpressionJoiner.h>
 #include <libyul/optimiser/ExpressionInliner.h>
 #include <libyul/optimiser/FullInliner.h>
+#include <libyul/optimiser/ForLoopConditionIntoBody.h>
 #include <libyul/optimiser/ForLoopInitRewriter.h>
 #include <libyul/optimiser/Rematerialiser.h>
 #include <libyul/optimiser/UnusedPruner.h>
@@ -93,6 +94,7 @@ void OptimiserSuite::run(
 	ControlFlowSimplifier{_dialect}(ast);
 	StructuralSimplifier{_dialect}(ast);
 	ControlFlowSimplifier{_dialect}(ast);
+	ForLoopConditionIntoBody{_dialect}(ast);
 	BlockFlattener{}(ast);
 
 	// None of the above can make stack problems worse.
